66ed2a759764346078e472155978a980ff682d6b
Daniela Oliveira <daniela.oliveira@skyline.be>
2026-07-10 14:19:14 +0100

    final cleanup

--- SDM.AssetManagementTests/Setup/ITestApiHelperExtensions.cs
+++ SDM.AssetManagementTests/Setup/ITestApiHelperExtensions.cs
@@ -77,7 +77,28 @@ namespace SDM.AssetManagement.Tests.Setup
                 throw new ArgumentNullException(nameof(helper));
             }
 
-            // Delete in reverse dependency order
+            CleanupRepos(helper);
+            CleanupCache(helper);
+        }
+
+        private static void CleanupCache(ITestApiHelper helper)
+        {
+            // Clear the test data cache
+            var cache = helper.TestData as TestDataCache;
+            if (cache != null)
+            {
+                cache.DeviceTypes = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.DeviceType>();
+                cache.AssetClasses = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.AssetClass>();
+                cache.Assets = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.Asset>();
+                cache.DataPorts = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.DataPort>();
+                cache.PowerPorts = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.PowerPort>();
+                cache.PortTypes = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.PortType>();
+                cache.Racks = Array.Empty<Skyline.DataMiner.SDM.FacilityManagement.Models.Rack>();
+            }
+        }
+
+        private static void CleanupRepos(ITestApiHelper helper)
+        {
             SafeDelete(() =>
             {
                 var portTypes = helper.AssetManagement.PortTypes.Read(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.PortType>());
@@ -140,19 +161,6 @@ namespace SDM.AssetManagement.Tests.Setup
                     helper.AssetManagement.DeviceTypes.Delete(deviceTypes);
                 }
             });
-
-            // Clear the test data cache
-            var cache = helper.TestData as TestDataCache;
-            if (cache != null)
-            {
-                cache.DeviceTypes = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.DeviceType>();
-                cache.AssetClasses = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.AssetClass>();
-                cache.Assets = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.Asset>();
-                cache.DataPorts = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.DataPort>();
-                cache.PowerPorts = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.PowerPort>();
-                cache.PortTypes = Array.Empty<Skyline.DataMiner.SDM.AssetManagement.Models.PortType>();
-                cache.Racks = Array.Empty<Skyline.DataMiner.SDM.FacilityManagement.Models.Rack>();
-            }
         }
 
         private static void SafeDelete(Action deleteAction)
@@ -165,93 +173,6 @@ namespace SDM.AssetManagement.Tests.Setup
             {
                 // Ignore cleanup errors - may fail if entity doesn't exist or has dependencies
             }
-        }
-
-        #endregion
-
-        #region Repository State Verification
-
-        /// <summary>
-        /// Checks if all repositories are empty (no data).
-        /// Useful for verifying clean state before/after tests.
-        /// </summary>
-        /// <param name="helper">The test API helper.</param>
-        /// <returns>True if all repositories are empty; otherwise false.</returns>
-        public static bool AreRepositoriesEmpty(this ITestApiHelper helper)
-        {
-            if (helper == null)
-            {
-                throw new ArgumentNullException(nameof(helper));
-            }
-
-            return helper.AssetManagement.Assets.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.Asset>()) == 0
-                && helper.AssetManagement.AssetClasses.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.AssetClass>()) == 0
-                && helper.AssetManagement.DeviceTypes.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.DeviceType>()) == 0
-                && helper.AssetManagement.DataPorts.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.DataPort>()) == 0
-                && helper.AssetManagement.PowerPorts.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.PowerPort>()) == 0
-                && helper.FacilityManagement.Racks.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.FacilityManagement.Models.Rack>()) == 0;
-        }
-
-        /// <summary>
-        /// Asserts that all repositories are empty, throwing an exception with details if not.
-        /// </summary>
-        /// <param name="helper">The test API helper.</param>
-        /// <exception cref="InvalidOperationException">Thrown when repositories are not empty, with details.</exception>
-        public static void AssertRepositoriesEmpty(this ITestApiHelper helper)
-        {
-            if (helper == null)
-            {
-                throw new ArgumentNullException(nameof(helper));
-            }
-
-            var nonEmptyRepositories = new List<string>();
-
-            var assetCount = helper.AssetManagement.Assets.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.Asset>());
-            if (assetCount > 0) nonEmptyRepositories.Add($"Assets ({assetCount})");
-
-            var assetClassCount = helper.AssetManagement.AssetClasses.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.AssetClass>());
-            if (assetClassCount > 0) nonEmptyRepositories.Add($"AssetClasses ({assetClassCount})");
-
-            var deviceTypeCount = helper.AssetManagement.DeviceTypes.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.DeviceType>());
-            if (deviceTypeCount > 0) nonEmptyRepositories.Add($"DeviceTypes ({deviceTypeCount})");
-
-            var dataPortCount = helper.AssetManagement.DataPorts.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.DataPort>());
-            if (dataPortCount > 0) nonEmptyRepositories.Add($"DataPorts ({dataPortCount})");
-
-            var powerPortCount = helper.AssetManagement.PowerPorts.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.PowerPort>());
-            if (powerPortCount > 0) nonEmptyRepositories.Add($"PowerPorts ({powerPortCount})");
-
-            var rackCount = helper.FacilityManagement.Racks.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.FacilityManagement.Models.Rack>());
-            if (rackCount > 0) nonEmptyRepositories.Add($"Racks ({rackCount})");
-
-            if (nonEmptyRepositories.Any())
-            {
-                throw new InvalidOperationException(
-                    $"Repositories are not empty. Non-empty repositories: {string.Join(", ", nonEmptyRepositories)}");
-            }
-        }
-
-        /// <summary>
-        /// Gets a dictionary of repository counts for diagnostic purposes.
-        /// </summary>
-        /// <param name="helper">The test API helper.</param>
-        /// <returns>Dictionary with repository names and their item counts.</returns>
-        public static Dictionary<string, long> GetRepositoryCounts(this ITestApiHelper helper)
-        {
-            if (helper == null)
-            {
-                throw new ArgumentNullException(nameof(helper));
-            }
-
-            return new Dictionary<string, long>
-            {
-                ["Assets"] = helper.AssetManagement.Assets.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.Asset>()),
-                ["AssetClasses"] = helper.AssetManagement.AssetClasses.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.AssetClass>()),
-                ["DeviceTypes"] = helper.AssetManagement.DeviceTypes.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.DeviceType>()),
-                ["DataPorts"] = helper.AssetManagement.DataPorts.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.DataPort>()),
-                ["PowerPorts"] = helper.AssetManagement.PowerPorts.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.AssetManagement.Models.PowerPort>()),
-                ["Racks"] = helper.FacilityManagement.Racks.Count(new Skyline.DataMiner.Net.Messages.SLDataGateway.TRUEFilterElement<Skyline.DataMiner.SDM.FacilityManagement.Models.Rack>())
-            };
         }
 
         #endregion
