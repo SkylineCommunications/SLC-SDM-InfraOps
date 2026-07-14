99b920cbd8140e4ef4d90b12e51f9bbb977a2ff7
Rafael Abranches <rafael.abranches@skyline.be>
2026-07-14 13:57:53 +0100

    fixed reference missing

--- SDM.AssetManagement.Common/Validation/AssetClass/AssetClassValidator.cs
+++ SDM.AssetManagement.Common/Validation/AssetClass/AssetClassValidator.cs
@@ -2,10 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using SharedMappers.DomIds;
 
-    using Skyline.DataMiner.SDM.AssetManagement.Common.Validation;
     using Skyline.DataMiner.SDM.AssetManagement.Models;
     using Skyline.DataMiner.SDM.Common.Services;
     using Skyline.DataMiner.SDM.Extensions;
