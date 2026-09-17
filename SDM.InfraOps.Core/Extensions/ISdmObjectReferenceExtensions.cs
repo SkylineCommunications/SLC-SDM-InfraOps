df6cf68d11f6eda07f6b7b91a2404ea4a0b75b5c
SKYLINE2\MiguelGD <miguel.garrido@skyline.be>
2026-09-17 10:29:14 +0100

    Improvements to ISDMObjectReference Equal

---
+++ SDM.InfraOps.Core/Extensions/ISdmObjectReferenceExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Skyline.DataMiner.SDM.Extensions
+{
+    using Skyline.DataMiner.Net.Messages.SLDataGateway;
+    using Skyline.DataMiner.SDM.InfraOps.Core.ApiReferences;
+
+    public static class ISdmObjectReferenceExtensions
+    {
+        public static ManagedFilter<TFilter, ISdmObjectReference<TField>> Equal<TFilter, TField>(
+            this Exposer<TFilter, ISdmObjectReference<TField>> exposer,
+            TField value)
+            where TFilter : class
+            where TField : ISdmObject
+        {
+            var reference = ISdmObjectReference<TField>.To(value);
+            return new ManagedFilter<TFilter, ISdmObjectReference<TField>>(exposer, Comparer.Equals, reference, delegate (TFilter obj)
+            {
+                var val = exposer.internalFunc(obj);
+                return val.HasValue() && val.Equals(reference);
+            });
+        }
+
+        public static ManagedFilter<TFilter, ISdmObjectReference<TField>> NotEqual<TFilter, TField>(
+            this Exposer<TFilter, ISdmObjectReference<TField>> exposer,
+            TField value)
+            where TFilter : class
+            where TField : ISdmObject
+        {
+            var reference = ISdmObjectReference<TField>.To(value);
+            return new ManagedFilter<TFilter, ISdmObjectReference<TField>>(exposer, Comparer.Equals, reference, delegate (TFilter obj)
+            {
+                var val = exposer.internalFunc(obj);
+                return val.HasValue() && !val.Equals(reference);
+            });
+        }
+    }
+}
